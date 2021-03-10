08348e314eda4e2e2846f6adaa8bd2e98e453564
robert.szeker <robert.szeker@movingimage.com>
2021-03-10 08:07:38 +0100

    snake

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,47 @@ fn init_display() -> Max7219 {
     return display;   
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Point {
     x: u8,
     y: u8,
+}
+
+struct Snake {
+    direction: Direction,
+    tail: Vec<Point>,
+}
+
+impl Snake {
+    fn change_direction(&mut self, direction: Direction) -> () {
+        self.direction = direction;
+    }
+
+    fn get_byte_rows_for_display(&self, display_index: usize) -> [u8; 8] {
+        let byte_row: u8 = 0b00000000;
+        let mut byte_rows = [byte_row; 8];
+
+        for point in self.tail {
+            for byte_row_index in 0..8 {
+                byte_rows[byte_row_index] = point.get_byte_rows_for_display(display_index)[byte_row_index] & byte_rows[byte_row_index];
+            }
+        }
+
+        byte_rows
+    }
+
+    fn walk(&mut self) -> () {
+        let mut head = Point{..self.tail[0]};
+        match self.direction {
+            Direction::Left => head.move_left(),
+            Direction::Right => head.move_right(),
+            Direction::Up => head.move_up(),
+            Direction::Down => head.move_down(),
+        };
+
+        self.tail.splice(0..0, vec![head].iter().copied());
+        self.tail.pop().expect("could not remove last element");
+    }
 }
 
 enum Direction {
@@ -85,45 +122,48 @@ impl Point {
 
 fn main() {
     let mut display = init_display();
-    let mut point = Point {
-        x: 31,
-        y: 4,
+
+    let point1 = Point { x: 8, y: 4 };
+    let point2 = Point { x: 9, y: 4 };
+    let point3 = Point { x: 10, y: 4 };
+    let mut snake = Snake {
+        direction: Direction::Left,
+        tail: vec![
+            point1,
+            point2,
+            point3,
+        ],
     };
 
     let stdout = stdout();
     let mut _stdout = stdout.lock().into_raw_mode().unwrap();
     let mut stdin = async_stdin().bytes();
-    let mut direction = Direction::Left;
 
     loop {
-        direction = match stdin.next() {
+        match stdin.next() {
             Some(Ok(27)) => {
                 match stdin.next() {
                     Some(Ok(91)) => {
                         match stdin.next() {
-                            Some(Ok(65)) => Direction::Up,
-                            Some(Ok(66)) => Direction::Down,
-                            Some(Ok(67)) => Direction::Right,
-                            Some(Ok(68)) => Direction::Left,
-                            _ => direction,
+                            Some(Ok(65)) => snake.change_direction(Direction::Up),
+                            Some(Ok(66)) => snake.change_direction(Direction::Down),
+                            Some(Ok(67)) => snake.change_direction(Direction::Right),
+                            Some(Ok(68)) => snake.change_direction(Direction::Left),
+                            _ => (),
                         }
                     }
-                    _ => direction,
+                    _ => (),
                 }
             },
             Some(Ok(b'q')) => break,
-            _ => direction,
+            _ => (),
         };
 
-        match direction {
-            Direction::Left => point.move_left(),
-            Direction::Right => point.move_right(),
-            Direction::Up => point.move_up(),
-            Direction::Down => point.move_down(),
-        }
+        // snake.walk();
 
         for i in 0..NUMBER_DISPLAYS {
-            display.write_raw(i, &point.get_byte_rows_for_display(i)).expect("couldn't write to display");
+            // display.write_raw(i, &point.get_byte_rows_for_display(i)).expect("couldn't write to display");
+            display.write_raw(i, &snake.get_byte_rows_for_display(i)).expect("couldn't write to display");
         }
     
         thread::sleep(Duration::from_millis(100));
